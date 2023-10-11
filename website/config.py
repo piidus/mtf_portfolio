45fe52d6b866f05ae7068e75bb6ce0d9b737541d
piidus <sudiipkumarbasu@gmail.com>
2023-10-11 13:25:19 +0530

    update to sqlite

--- website/config.py
+++ website/config.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'hardsecretkey'
 db_user = os.getenv('DB_USER')
 db_pass = os.getenv('DB_PASS')
 db_name = os.getenv('DB_NAME')
-# app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-SQLALCHEMY_DATABASE_URI = f'mysql://{db_user}:{db_pass}@localhost:3306/{db_name}'
+SQLALCHEMY_DATABASE_URI = f'sqlite:///database.db'
+# SQLALCHEMY_DATABASE_URI = f'mysql://{db_user}:{db_pass}@localhost:3306/{db_name}'
 # 'mysql://username:password@localhost/db_name'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
