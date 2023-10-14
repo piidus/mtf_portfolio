a4e50a657cc9b18bda654c3eae83f61f77f8a752
piidus <sudiipkumarbasu@gmail.com>
2023-10-14 18:01:02 +0530

    update sql to environ

--- website/config.py
+++ website/config.py
@@ -1,12 +1,12 @@
 import os, dotenv
-dotenv.load_dotenv('.env')
+dotenv.load_dotenv()
 DEBUG = True
 # SQLALCHEMY_DATABASE_URI =
 SECRET_KEY = 'hardsecretkey'
-db_user = os.getenv('DB_USER')
-db_pass = os.getenv('DB_PASS')
-db_name = os.getenv('DB_NAME')
-db_path = os.getenv('DB_PATH')
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASS')
+db_name = os.environ.get('DB_NAME')
+db_path = os.environ.get('DB_PATH')
 # SQLALCHEMY_DATABASE_URI = f'sqlite:///database.db'
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_pass}@{db_path}:3306/{db_name}'
 # 'mysql://username:password@localhost/db_name'
