9b5368853e2ccb364f7d51d488183774695433f2
piidus <sudiipkumarbasu@gmail.com>
2023-10-16 17:21:55 +0530

    check

--- website/config.py
+++ website/config.py
@@ -1,8 +1,8 @@
 import os, dotenv
-dotenv.load_dotenv()
+# dotenv.load_dotenv()
 DEBUG = True
 # from test import db, db_pwd, db_user1, db_path1
-print('fake')
+# print('fake')
 # SQLALCHEMY_DATABASE_URI =
 SECRET_KEY = 'hardsecretkey'
 db_user = os.environ.get('DB_USER')
