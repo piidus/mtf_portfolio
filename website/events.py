f2235c002f0b2fc728a74d54dc74672aebb86b06
piidus <sudiipkumarbasu@gmail.com>
2023-10-18 17:04:55 +0530

    adjust notification

--- website/events.py
+++ website/events.py
@@ -1,11 +1,26 @@
 from flask_socketio import SocketIO
 from flask_socketio import emit
+import random, time
 socketio = SocketIO()
 @socketio.on('connect')
 def socket_connect():
     print('Client Connected')
 
+
+
+def generate_random_numbers():
+    total =2
+    while total>0:
+        time.sleep(1)  # Adjust this delay based on your needs
+        num = random.randint(1, 1000)
+        print(num)
+        if num % 17 == 0:
+            print(num)
+            emit('random_number', num)
+            total-=1
+
 @socketio.on('message')
 def handle_message(message):
     print(message)
     emit('response', message+'hello')  # Example WebSocket event handler
+    generate_random_numbers()
--- website/static/test.js
+++ website/static/test.js
@@ -10,6 +10,31 @@ socket.on('connect', function() {
     alert('I am connected');
 });
 
-socket.on('response', function(data) {
-    console.log(data);
+// socket.on('response', function(data) {
+//     console.log(data);
+// });
+
+function requestNotificationPermission() {
+    Notification.requestPermission().then(function(permission) {
+        if (permission === 'granted') {
+            console.log('Notification permission granted.');
+        } else {
+            console.log('Notification permission denied.');
+        }
+    });
+}
+
+
+socket.on('random_number', function(num) {
+    console.log('Received random number:', num);
+    showNotification('Random Number', `Generated number: ${num}`);
 });
+
+function showNotification(title, message) {
+    if (Notification.permission === 'granted') {
+        var options = {
+            body: message
+        };
+        var notification = new Notification(title, options);
+    }
+}
