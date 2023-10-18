f7a78b10bf14105553aa1b46d034a34849164b78
piidus <sudiipkumarbasu@gmail.com>
2023-10-18 17:15:12 +0530

    add even test

--- website/static/test.js
+++ website/static/test.js
@@ -24,6 +24,7 @@ function requestNotificationPermission() {
     });
 }
 
+socket.om('even', function(num){console.log(num)});
 
 socket.on('random_number', function(num) {
     console.log('Received random number:', num);
